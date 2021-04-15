3bcefa1d5cb15776ec1fa9ca5c5e77fdb2657e2a
Grace Barna <gbarna@umich.edu>
2021-04-15 11:36:02 -0400

    Logout button takes user to the login screen

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Text } from 'react-native';
 import { DrawerContentScrollView } from '@react-navigation/drawer';
 import { styles } from '../styles/Styles';
+import { Button } from 'react-native';
 
 const SideMenu = (props) => {
   return (
     <DrawerContentScrollView {...props}>
       <Text>Edit Account</Text>
-      <Text>Log Out</Text>
+      <Button title="Log out" onPress={() => props.navigation.navigate("Login")} />
     </DrawerContentScrollView>
   );
 };
