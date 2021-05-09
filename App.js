edbfaa28d579e667e77a00acd65ecd7a26ded811
Caroline Ham <cyham@umich.edu>
2021-05-08 22:12:29 -0400

    Connect to Firebase

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigation from './src/components/DrawerNavigation';
+import * as firebase from 'firebase/app';
+import { LogBox } from 'react-native';
 
 const App = () => {
+
+  // Supress warning about timer that is caused when connecting to firebase
+  // It is a bug so it can safely be supressed:
+  // https://stackoverflow.com/questions/44603362/setting-a-timer-for-a-long-period-of-time-i-e-multiple-minutes/48778011#48778011
+  LogBox.ignoreLogs(['Setting a timer']);
+
+  useEffect(() => {
+    var firebaseConfig = {
+      apiKey: "AIzaSyAKuVcL0xLDktqHubVs6uC0Lhm_i-P4F5Q",
+      authDomain: "healthy-latina-lifestyle.firebaseapp.com",
+      databaseURL: "https://healthy-latina-lifestyle-default-rtdb.firebaseio.com",
+      projectId: "healthy-latina-lifestyle",
+      storageBucket: "healthy-latina-lifestyle.appspot.com",
+      messagingSenderId: "860970927043",
+      appId: "1:860970927043:web:329d3f22ef2434884f3376",
+      measurementId: "G-JZXQPQ4TRM"
+    };
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    else {
+      firebase.app(); // if already initialized, use that one
+    }
+  }, []);
+
   return (
     <NavigationContainer>
       <DrawerNavigation/>
