05043376cee1d27a3d8fd6d1c04bcea88bce1957
Caroline Ham <cyham@umich.edu>
2021-06-29 17:17:31 -0400

    #22 - Only load groups the current user is part of; update list when group is created

--- src/screens/ChatRoomHome.js
+++ src/screens/ChatRoomHome.js
@@ -1,169 +1,131 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { View, FlatList, TouchableOpacity } from 'react-native';
 import { List, Divider } from 'react-native-paper';
+import LoadingSpinner from '../components/LoadingSpinner';
 import { styles } from '../styles/Styles';
-import firebase, { auth } from 'firebase';
+import firebase from 'firebase';
 import { Button } from 'react-native-elements';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-
 const ChatRoomHome =(props)=> {
   const [threads, setThreads] = useState([]);
-  const [usersGroups, setUserGroups] = useState([]);
-  const [finalGroups, setFinalGroups] = useState([]);
-  const [index, setIndex] = useState(0);
-const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))}
+  const [usersGroups, setUsersGroups] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-
-
-
-async function CreateRoom(){
-var user = firebase.auth().currentUser;
-var docID;
-await firebase.firestore().collection("Groups").add({
-name: 'Room ' + threads.length,
-message:'default'
-})
-.then(docRef => docID = docRef.id);
-await firebase.firestore().collection("Users").doc(user.uid).set({
-GroupID:firebase.firestore.FieldValue.arrayUnion(docID)
-}, {merge:true});
-}
-//right now it grabs all the groups. i need to figure out how to seperate to ones only had by the user
-//im thinking by grabbing the array from the user, i can delete any entry from the threads array that arent in my users array
-async function compareArrays(){
-console.log("enter comapre");
-var indexSmall = 0;
-var stuff = [];
-console.log(usersGroups.length);
-if(threads.length > 0)
-{
-//let index = 0;
-  for(let i = 0; i< threads.length; i++)
-  {
-  console.log("entered first loop");
-        for(let j = 0; j < usersGroups.length; i++)
-        {
-        console.log("enter second loop");
-
-        if((threads.map(x=>x._id)[i]) === usersGroups[j])
-        {
-
-        stuff[indexSmall] = (threads.map(x=>x._id)[i]);
-        //console.log(finalGroups[index]);
-        console.log("enter if");
-        indexSmall++;
+  const fetchUsersGroups = async () => {
+    const dbh = firebase.firestore();
+    dbh.collection("Users").doc(firebase.auth().currentUser.uid).get()
+    .then((doc) => {
+      if (doc.exists) {
+        if (!doc.data().GroupID || doc.data().GroupID.length == 0) {
+          setIsLoaded(true);
         }
+        else {
+          setUsersGroups(doc.data().GroupID);
         }
-        }
-  }
-  else if(threads.length === 0)
-  {
-  }
-  //console.log("final groups");
-  //console.log(finalGroups);
-  setFinalGroups(stuff);
-  setIndex(index+1);
-  }
-
-
-async function loadUserRooms()
-    {
-   var user =  await firebase.auth().currentUser
-   //console.log("oof");
-
-     await firebase.firestore().collection('Users').doc(user.uid).get()
-    .then((documentSnapshot) =>{
-     const stuff = getGroups(documentSnapshot);
-    setUserGroups(stuff);
-    //console.log(usersGroups[0]);
+      } 
+      else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
     });
-    compareArrays();
+  };
+
+  const fetchRooms = async () => {
+    if (usersGroups.length == 0) {
+      fetchUsersGroups();
     }
-     function getGroups(documentSnapshot){
-                return documentSnapshot.get('GroupID');
-
-                }
-
-
-  useEffect(() => {
-
-
-    const unsubscribe =
-    firebase.firestore()
-      .collection('Groups')
-      .onSnapshot((querySnapshot) => {
-        const stuff = querySnapshot.docs.map((documentSnapshot) => {
-          return {
-            _id: documentSnapshot.id,
-            // give defaults
-            name: '',
-            ...documentSnapshot.data(),
-
-          };
-        });
-
-        setThreads(stuff);
-        if(threads.length>0)
-        {
-        loadUserRooms();
+    else
+    {
+      const dbh = firebase.firestore();
+      // Code from: https://stackoverflow.com/questions/62524946/firestore-check-if-id-exists-against-an-array-of-ids
+      dbh.collection("Groups").where(firebase.firestore.FieldPath.documentId(), "in", usersGroups).get()
+      .then((querySnapshot) => {
+        if (querySnapshot.size == 0) {
+          setIsLoaded(true);
         }
-        else
-        {
-        setIndex(index+1);
+        else {
+          let tempGroupsArray = [];
+          querySnapshot.forEach((doc) => {
+            let newDoc = doc.data();
+            newDoc._id = doc.id;
+            tempGroupsArray.push(newDoc);
+          });
+          setThreads(tempGroupsArray);
         }
-       console.log("entered page");
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      })
+      .finally(() => {
+        setIsLoaded(true);
       });
+    }
+  };
 
+  async function CreateRoom() {
+    var user = firebase.auth().currentUser;
+    var docID;
+    await firebase.firestore().collection("Groups").add({
+      name: 'Room ' + threads.length,
+      message:'default'
+    })
+    .then(docRef => docID = docRef.id);
 
+    await firebase.firestore().collection("Users").doc(user.uid).set({
+      GroupID:firebase.firestore.FieldValue.arrayUnion(docID)
+    }, {merge:true});
 
-    /**
-     * unsubscribe listener
-     */
+    // Update usersGroups to be empty, so the new group will appear on the list.
+    // Changing usersGroups will trigger useEffect, so fetchRooms will be called.
+    // This function calls fetchUsersGroups when usersGroups is empty which will update the list.
+    setIsLoaded(false);
+    setUsersGroups([]);
+  }
 
-     return() => unsubscribe(); //compareArrays();
+  useEffect(() => { 
+    fetchRooms();
+  }, [usersGroups]);
 
+  return(
+    <View style={styles.app}>
+      { isLoaded ?
+        <View>
+          <FlatList
+            data={threads}
+            keyExtractor={(item) => item._id}
+            ItemSeparatorComponent={() => <Divider />}
+            renderItem={({ item }) => (
 
-
-
-
-  }, []);
-
-
-return(
-<View style={styles.container}>
-  <FlatList
-    data={threads}
-    keyExtractor={(item) => item._id}
-    ItemSeparatorComponent={() => <Divider />}
-    renderItem={({ item }) => (
-
-      <TouchableOpacity
-      onPress= {() => props.navigation.navigate("ChatScreen", {thread:item})}
-      >
-      <List.Item
-        title={item.name}
-        //description='Item description'
-        titleNumberOfLines={1}
-        titleStyle={styles.listTitle}
-        //descriptionStyle={styles.listDescription}
-        //descriptionNumberOfLines={1}
-      />
-      </TouchableOpacity>
-    )}
-  />
-  <View>
-  <Button
-                        buttonStyle={styles.button}
-                        titleStyle={styles.buttonText}
-                        title="Create Room" onPress={() => CreateRoom()}
-                        />
-             </View>
-</View>
-
-);
+              <TouchableOpacity
+              onPress= {() => props.navigation.navigate("ChatScreen", {
+                thread: item
+              })}
+              >
+              <List.Item
+                title={item.name}
+                //description='Item description'
+                titleNumberOfLines={1}
+                titleStyle={styles.listTitle}
+                //descriptionStyle={styles.listDescription}
+                //descriptionNumberOfLines={1}
+              />
+              </TouchableOpacity>
+            )}
+          />
+          <Button
+            buttonStyle={styles.button}
+            titleStyle={styles.buttonText}
+            title="Create Room" onPress={() => CreateRoom()}
+            />
+        </View>
+      : <LoadingSpinner />
+      }
+      </View>
+  );
   // ...rest of the component
 }
 export default ChatRoomHome;
