c02b5ad30a360db0b69bca33d77e421fbca2b86e
Caroline Ham <cyham@umich.edu>
2021-07-29 14:22:22 -0400

    QA Work - Updated and fixed snapshot tests, disabled the Challenge tests for now

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,5 @@ jest.mock('react-native-vector-icons');
 jest.mock('react-native-modal-datetime-picker/src/Modal');
 jest.mock('@expo/vector-icons');
 jest.mock('react-native-vector-icons/FontAwesome');
+jest.mock('react-native-vector-icons/MaterialIcons');
+jest.useFakeTimers();
