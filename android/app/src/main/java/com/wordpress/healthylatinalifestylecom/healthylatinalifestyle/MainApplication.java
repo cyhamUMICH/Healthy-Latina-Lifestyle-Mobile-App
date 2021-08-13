a3e34d84f61730d16c5732ab820986ec12121951
Caroline Ham <cyham@umich.edu>
2021-08-12 20:38:34 -0400

    Fix running on Android?

--- android/app/src/main/java/com/wordpress/healthylatinalifestylecom/healthylatinalifestyle/MainApplication.java
+++ android/app/src/main/java/com/wordpress/healthylatinalifestylecom/healthylatinalifestyle/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 import com.wordpress.healthylatinalifestylecom.healthylatinalifestyle.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
